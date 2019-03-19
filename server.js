0aa34698871844695e83f29b82b4ad1433abedd0
Michael Stepanov <michael@stepanoff.org>
2019-03-19 13:03:13 +0200

    Improved logic to send messages to the connected clients

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-const WebSocket = require('ws');
+const WSS = require('ws').Server;
 
-const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 8080;
 
-// Broadcast to all.
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
+const server = new WSS({ port: PORT });
+
+console.log('Listen port ' + PORT + ' ...');
+
+let delay = 1000;
+server.on('connection', (socket) => {
+  console.log('Opened connection ');
+
+  var json = JSON.stringify({ message: 'Hi' });
+  socket.send(json);
+    
+  let timer = setTimeout(broadcast, delay);
+
+  socket.on('message', (message) => {
+    console.log('Received: ' + message);
   });
-};
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
+  socket.on('close', () => {
+    console.log('Closed Connection ');
   });
+
 });
+
+const min = 1;
+const max = 5;
+const multiplier = 1000;
+
+const broadcast = () => {
+  let json = JSON.stringify({
+    value: 'any string'
+  });
+
+  server.clients.forEach(function each(client) {
+    client.send(json);
+    console.log('Sent: ' + json);
+  });
+   
+  delay = Math.floor((Math.random() * max) + min) * multiplier;
+
+  console.log("Delay is " + delay);
+  timer = setTimeout(broadcast, delay);
+}
+
